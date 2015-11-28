6dbed052b6c22dd4374de263133bd7372d2d37a8
WilliamLewww <WilliamLew32@gmail.com>
2015-11-27 16:14:18 -0800

    initial

---
+++ Monogame Font Implementation/FontSeparation.cs
@@ -0,0 +1,88 @@
+﻿using System.Drawing;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FontSeparation
+{
+    class FontSeparation
+    {
+        static Bitmap font;
+        public List<int[]> coordinateList = new List<int[]>();
+
+        public void LoadBitmap(Bitmap bitmap)
+        {
+            font = bitmap;
+
+            bool a = false;
+            int minX = -1, minY = bitmap.Height, maxX = 0, maxY = 0;
+
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                a = false;
+
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    if (bitmap.GetPixel(x, y).A != 0)
+                    {
+                        a = true;
+
+                        if (minX == -1)
+                            minX = x;
+
+                        if (y < minY)
+                            minY = y;
+
+                        if (x > maxX)
+                            maxX = x;
+
+                        if (y > maxY)
+                            maxY = y;
+                    }
+                }
+
+                if (a == false)
+                {
+                    if (minX != -1)
+                        coordinateList.Add(new int[4] { minX, minY, (maxX + 1) - minX, (maxY + 1) - minY });
+
+                    minX = -1;
+                    minY = bitmap.Height;
+                    maxX = 0;
+                    maxY = 0;
+                }
+            }
+        }
+    }
+
+    class Character
+    {
+        Texture2D texture;
+        FontSeparation fontSeparation;
+
+        public static List<Microsoft.Xna.Framework.Rectangle> characterList = new List<Microsoft.Xna.Framework.Rectangle>();
+
+        private static ContentManager content;
+        public static ContentManager Content
+        {
+            set { content = value; }
+        }
+
+        public Character(string image)
+        {
+            texture = content.Load<Texture2D>("font.png");
+
+            fontSeparation = new FontSeparation();
+            fontSeparation.LoadBitmap(new Bitmap(image));
+
+            foreach (int[] coordinate in fontSeparation.coordinateList)
+                characterList.Add(new Microsoft.Xna.Framework.Rectangle(coordinate[0], coordinate[1], coordinate[2], coordinate[3]));
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, new Vector2(0, 0), characterList[0], Microsoft.Xna.Framework.Color.White);
+        }
+    }
+}
---
+++ Monogame Font Implementation/Game1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame_Font_Implementation
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        FontSeparation.Character character;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            FontSeparation.Character.Content = Content;
+            character = new FontSeparation.Character("font.png");
+        }
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            character.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
