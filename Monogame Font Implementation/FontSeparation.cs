17197b05778f11f8f0dffbd5056dfe0b6cc5998d
WilliamLewww <WilliamLew32@gmail.com>
2015-11-27 23:47:15 -0800

    whoops

--- Monogame Font Implementation/FontSeparation.cs
+++ Monogame Font Implementation/FontSeparation.cs
@@ -61,7 +61,7 @@ namespace FontSeparation
         Texture2D texture;
         FontSeparation fontSeparation;
 
-        int OFFSET = 0;
+        int OFFSET = 10;
 
         public static List<Microsoft.Xna.Framework.Rectangle> characterList = new List<Microsoft.Xna.Framework.Rectangle>();
         List<Microsoft.Xna.Framework.Rectangle> newCharacterList = new List<Microsoft.Xna.Framework.Rectangle>();
